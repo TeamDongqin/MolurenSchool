0be2ff7b3b8df513967cfea61e858571700af058
dongqin03 <lingshan.zheng.i@icloud.com>
2015-04-05 14:55:49 +0800

    Initial Commit

---
+++ MolurenSchool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MolurenSchool
+//
+//  Created by zheng lingshan on 15/4/5.
+//  Copyright (c) 2015年 TeamDQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MolurenSchool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MolurenSchool
+//
+//  Created by zheng lingshan on 15/4/5.
+//  Copyright (c) 2015年 TeamDQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
