54c2110c41a16a524a3172c54a4c0bc86671a9db
dongqin03 <lingshan.zheng.i@icloud.com>
2015-04-08 09:18:24 +0800

    1) Add libs folder and libs. 2) Add TestXmpp class

---
+++ MolurenSchool/Model/TestXmpp.h
@@ -0,0 +1,17 @@
+//
+//  TestXmpp.h
+//  MolurenSchool
+//
+//  Created by zheng lingshan on 15/4/8.
+//  Copyright (c) 2015年 TeamDQ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestXmpp : NSObject
+
+-(void)TestAddUser;
+-(void)TestDeleteUser;
+-(void)TestChat;
+
+@end
---
+++ MolurenSchool/Model/TestXmpp.m
@@ -0,0 +1,25 @@
+//
+//  TestXmpp.m
+//  MolurenSchool
+//
+//  Created by zheng lingshan on 15/4/8.
+//  Copyright (c) 2015年 TeamDQ. All rights reserved.
+//
+
+#import "TestXmpp.h"
+
+@implementation TestXmpp
+
+-(void)TestAddUser{
+    
+}
+
+-(void)TestDeleteUser{
+    
+}
+
+-(void)TestChat{
+    
+}
+
+@end
