843dd9a13c41343d9dfe5962ae7f1d0d6e2730ac
dongqin03 <lingshan.zheng.i@icloud.com>
2015-04-12 11:46:14 +0800

    1) Add TdFindFriendViewController

---
+++ MolurenSchool/Controller/TdFindFriendViewController.h
@@ -0,0 +1,13 @@
+//
+//  TdFindFriendViewController.h
+//  MolurenSchool
+//
+//  Created by zheng lingshan on 15/4/12.
+//  Copyright (c) 2015年 TeamDQ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TdFindFriendViewController : NSObject
+
+@end
---
+++ MolurenSchool/Controller/TdFindFriendViewController.m
@@ -0,0 +1,13 @@
+//
+//  TdFindFriendViewController.m
+//  MolurenSchool
+//
+//  Created by zheng lingshan on 15/4/12.
+//  Copyright (c) 2015年 TeamDQ. All rights reserved.
+//
+
+#import "TdFindFriendViewController.h"
+
+@implementation TdFindFriendViewController
+
+@end
