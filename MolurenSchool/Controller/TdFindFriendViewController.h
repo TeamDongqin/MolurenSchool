bf43812fb1aa7f8e14c8ce6f398edc0cf3e4bfe9
JSGTL <giantliu@yeah.net>
2015-04-15 01:30:33 +0800

    add TdFindFriendViewController and dependence

--- MolurenSchool/Controller/TdFindFriendViewController.h
+++ MolurenSchool/Controller/TdFindFriendViewController.h
@@ -6,8 +6,31 @@
 //  Copyright (c) 2015年 TeamDQ. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "PullTableView.h"
 
-@interface TdFindFriendViewController : NSObject
+@interface TdFindFriendViewController : UIViewController<UITableViewDelegate,UITableViewDataSource,UISearchDisplayDelegate,UISearchBarDelegate>
+
+{
+    NSMutableArray * _friendArray;
+    NSString * searchKey;
+    
+    IBOutlet UITableView *_friendTable;
+    
+    IBOutlet UIWebView * web;
+    
+    IBOutlet UIView *findView;
+    int _pageIndex;
+    int _pageSize;
+    
+//    IBOutlet UIImageView *refreshView;
+}
+
+- (IBAction)searchUser:(id)sender;
+- (IBAction)closeFind:(id)sender;
+- (IBAction)webBack:(id)sender;
+
+///最后加载的SEL
+- (void)refrenshDataWithLastSEL;
 
 @end
--- MolurenSchool/Controller/TdFindFriendViewController.m
+++ MolurenSchool/Controller/TdFindFriendViewController.m
@@ -8,6 +8,123 @@
 
 #import "TdFindFriendViewController.h"
 
+@interface TdFindFriendViewController()
+
+@end
+
 @implementation TdFindFriendViewController
+
+
+-(id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
+    
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if(self){
+        //Custom initialization
+    }
+    
+    return self;
+}
+
+-(void)viewDidLoad{
+    
+    [super viewDidLoad];
+    
+    _friendArray = [[NSMutableArray alloc] init];
+    
+    self.navigationItem.title = @"最新注册用户";
+    _pageIndex = 1;
+    searchKey = @"";
+    
+    [self findFriends];
+    
+//    [_friendTable registerNib:[UINib nibWithNibName:@"WCFriendCell" bundle:nil] forCellReuseIdentifier:@"friendCell"];
+//    [self.searchDisplayController.searchResultsTableView registerNib:[UINib nibWithNibName:@"WCFriendCell" bundle:nil] forCellReuseIdentifier:@"friendCell"];
+    
+}
+
+-(void)findFriends{
+    /* 搜索好友接口 */
+//    ASIFormDataRequest *request=[ASIFormDataRequest requestWithURL:API_BASE_URL(@"findFriend.do")];
+//    
+//    [request setPostValue:[[NSUserDefaults standardUserDefaults]objectForKey:kMY_API_KEY ] forKey:@"apiKey"];
+//    [request setPostValue:[NSNumber numberWithInt:_pageIndex] forKey:@"pageIndex"];
+//    [request setPostValue:[NSNumber numberWithInt:10] forKey:@"pageSize"];
+//    [request setPostValue:searchKey forKey:@"nickName"];
+//    [MMProgressHUD showWithTitle:@"查找好友" status:@"请求中..." ];
+//    [request setCompletionBlock:^{
+//        NSLog(@"response:%@",request.responseString);
+//        SBJsonParser *paser=[[[SBJsonParser alloc]init]autorelease];
+//        NSDictionary *rootDic=[paser objectWithString:request.responseString];
+//        int resultCode=[[rootDic objectForKey:@"status"]intValue];
+//        if (resultCode==1) {
+//            [MMProgressHUD dismissWithSuccess:[rootDic objectForKey:@"msg"] title:@"查找好友成功" afterDelay:0.75f];
+//            //保存账号信息
+//            NSArray *userArr=[rootDic objectForKey:@"userList"];
+//            
+//            for (NSDictionary *dic in userArr) {
+//                [_friendsArray addObject:dic];
+//                WCUserObject *user=[WCUserObject userFromDictionary:dic];
+//                if (![WCUserObject haveSaveUserById:user.userId]) {
+//                    [WCUserObject saveNewUser:user];
+//                }
+//            }
+//            [_friendTable reloadData];
+//            [self.searchDisplayController.searchResultsTableView reloadData];
+//            
+//            
+//            
+//        }else
+//        {
+//            [MMProgressHUD dismissWithError:[rootDic objectForKey:@"msg"] title:@"查找好友失败" afterDelay:0.75f];
+//        }
+//        
+//    }];
+//    
+//    [request setFailedBlock:^{
+//        [MMProgressHUD dismissWithError:@"链接服务器失败！" title:@"查找好友失败" afterDelay:0.75f];
+//    }];
+//    
+//    [request startAsynchronous];
+}
+
+#pragma TableView协议    ----------TableView协议---------
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return _friendArray.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return nil;
+}
+
+#pragma TableView协议
+
+-(void)didReceiveMemoryWarning{
+    
+    [super didReceiveMemoryWarning];
+}
+
+
+-(void)refrenshDataWithLastSEL{
+    
+}
+
+-(IBAction)searchUser:(id)sender{
+
+}
+
+-(IBAction)closeFind:(id)sender{
+    
+}
+
+-(IBAction)webBack:(id)sender{
+    
+}
 
 @end
